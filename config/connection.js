8ed5602d93b042f34e048eb3f0f00c9146eaa986
Brayden <banjosquash@gmail.com>
2022-11-10 10:17:11 -0500

    trying to get heroku working

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,18 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 //connect database
-const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // create connection to our database 
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
     });
+}
 
-    module.exports = sequelize
+
+module.exports = sequelize;
